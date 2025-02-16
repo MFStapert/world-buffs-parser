87910448f9b08891807ba88c0d1be0506d70dab4
marijn stapert <m.stapert@codefield.com>
2025-02-16 20:40:15 +0100

    generate site

--- src/models/report.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ReportModel {
-    playerName: string;
-    playerId: number;
-    dragonSlayer: number;
-    songFlower: number;
-    moldar: number;
-    fengus: number;
-    slipKik: number;
-}
--- src/models/world-buffs.model.ts
+++
@@ -1,7 +0,0 @@
-export const worldBuffs = {
-    22817: "moldar",
-    22818: "fengus",
-    22820: "slipKik",
-    355363: "dragonSlayer",
-    15366: "songFlower"
-};
